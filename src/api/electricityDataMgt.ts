4c3ff2342ca01fef02be2a14a1b7da8a2c43e37e
KevinWang <kevinwang2020@sina.com>
2023-12-03 01:03:19 +0800

    feat: 调整文件名为小写字母开头

---
+++ src/api/electricityDataMgt.ts
@@ -0,0 +1,24 @@
+import request from './request'
+
+// 设备列表
+const getEquipmentList = (params: { projectId: number }) =>
+  request({ url: '/sys/buildinginfo/tree', method: 'get', params })
+
+type ElectricityDataPayload = {
+  startTime: string
+  endTime: string
+  buildingId: string
+  energytype: string
+  datetype: string
+  functiontype: string
+  projectId: string
+  pageNum: string
+  pageSize: string
+}
+
+// 电力数据详情
+const getElectricityDataByType = (data: ElectricityDataPayload) =>
+  request({ url: '/eceraw/datalist', method: 'post', data })
+
+export { getElectricityDataByType, getEquipmentList }
+export type { ElectricityDataPayload }
---
+++ src/api/environmentDataMgt.ts
@@ -0,0 +1,20 @@
+import request from './request'
+
+type EnvironmentDataPayload = {
+  startTime: string
+  endTime: string
+  buildingId: string
+  energytype: string
+  datetype: string
+  functiontype: string
+  projectId: string
+  pageNum: string
+  pageSize: string
+}
+
+// 环境数据详情
+const getEnvironmentDataByType = (data: EnvironmentDataPayload) =>
+  request({ url: '/eceraw/datalist', method: 'post', data })
+
+export { getEnvironmentDataByType }
+export type { EnvironmentDataPayload }
