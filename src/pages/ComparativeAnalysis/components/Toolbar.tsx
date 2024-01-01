a53e66419304b2d36ef87d449e741e1233710354
KevinWang <kevinwang2020@sina.com>
2024-01-01 10:57:42 +0800

    fix: 修复时间切换计算错误

--- src/pages/ComparativeAnalysis/components/Toolbar.tsx
+++ src/pages/ComparativeAnalysis/components/Toolbar.tsx
@@ -67,14 +67,12 @@ const Toolbar: React.FC = () => {
   const onDateChange: DatePickerProps['onChange'] = (date: Dayjs | null) => {
     setDate(date)
     const dateRange = pickerType === 'date' ? 'day' : pickerType
-    const startDateformat = pickerType === 'date' ? 'YYYY-MM-DD HH:mm:ss' : 'YYYY-MM-DD 00:00:00'
-    const endDateformat = pickerType === 'date' ? 'YYYY-MM-DD HH:mm:ss' : 'YYYY-MM-DD 24:00:00'
+    const startDateformat = 'YYYY-MM-DD 00:00:00'
+    const endDateformat = 'YYYY-MM-DD 24:00:00'
     const startTime =
-      dateRange === 'day'
-        ? dayjs(date).add(-1, 'day').format(startDateformat)
-        : dayjs(date).startOf(dateRange).format(startDateformat)
+      dateRange === 'day' ? dayjs(date).format(startDateformat) : dayjs(date).startOf(dateRange).format(startDateformat)
     const endTime =
-      dateRange === 'day' ? dayjs(date).format(startDateformat) : dayjs(date).endOf(dateRange).format(endDateformat)
+      dateRange === 'day' ? dayjs(date).format(endDateformat) : dayjs(date).endOf(dateRange).format(endDateformat)
 
     actions.onSearch({
       startTime,
